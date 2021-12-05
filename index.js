39a10df33bf929d18256555689a6225acc9b4f86
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-12-05 14:37:07 -0800

    commit tree f07f2fd4ea445412e2b0b6097a60530110f5e22a

--- index.js
+++ index.js
@@ -11,6 +11,47 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function capitalizeFirstLetter(name) {
+  let newName = ''
+  
+  for (let i = 0; i < name.length; i++) {
+    
+    if (i === 0){
+      newName = newName.concat(name[i].toUpperCase())
+    }
+    else if (name[i - 1] === ' '){
+      newName = newName.concat(name[i].toUpperCase())
+    }
+    else {
+      newName = newName.concat(name[i])
+    }
+  }
+  return newName
 }
+
+
+/*
+function capitalizeWord (word){
+  return word.substring(0,1).toUpperCase() + word.slice(1)
+}
+
+function capitalizeFirstLetter(sentance) {
+  const wordArray = sentance.split(' ')
+  const capArray = wordArray.map(capitalizeWord)
+  const sentance2 = capArray.join(' ')
+  return sentance2
+
+}
+
+*/
+
+
+const titleCased = () => {
+  return tutorials.map(capitalizeFirstLetter)
+}
+
+
+console.log(titleCased(tutorials))
+
+
